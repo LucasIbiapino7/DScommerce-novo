8fd691b2e6c5670da4e07c5d7c1b2ba6f24471bf
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2024-01-14 13:15:20 -0300

    Exception Handling

---
+++ src/main/java/com/devsuperior/DScommercenovo/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.DScommercenovo.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
