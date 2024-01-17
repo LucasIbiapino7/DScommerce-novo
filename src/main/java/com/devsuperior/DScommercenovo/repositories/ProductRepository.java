4569e1c9e2354199025fb081b6007d2626e14d35
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2024-01-17 14:34:13 -0300

    Search by Name paginado

--- src/main/java/com/devsuperior/DScommercenovo/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/DScommercenovo/repositories/ProductRepository.java
@@ -1,10 +1,18 @@
 package com.devsuperior.DScommercenovo.repositories;
 
 import com.devsuperior.DScommercenovo.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     //<Tipo da entidade, tipo da chave primária da entidade>
+
+    @Query("SELECT obj " +
+            "FROM Product obj " +
+            "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Product> searchByName(String name, Pageable pageable);
 
 }
